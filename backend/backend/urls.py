84d7c65f2e635e943283fa2d896b6e85ea4208df
Rafael Hochedlinger <rafael.hochedlinger@gmail.com>
2024-12-12 10:57:06 +0100

    Add swagger

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,28 +14,40 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
 from django.contrib import admin
 from django.urls import path, include
-# from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+from drf_yasg import openapi
 
+class AdminOnlyPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_staff
 
 API_TITLE = 'BeerHub'
 API_DESCRIPTION = 'A nice web API for beer'
 
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title=API_TITLE,
+        default_version='v1',
+        description=API_DESCRIPTION
+    ),
+    public=True,
+    permission_classes=(AdminOnlyPermission,),
+)
+
 urlpatterns = [
     path('admin-kelRtItCHrav/', admin.site.urls),
 
-    # there is a bug with coreapi
-    # path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
-
     path('schema/', get_schema_view(title=API_TITLE)),
 
-    path('auth/', include('rest_framework.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
+    path('auth/', include('rest_framework.urls')),
     path('api/v1/beers/', include('beers.urls')),
     path('api/v1/auth/', include('dj_rest_auth.urls')),
     path('api/v1/auth/registration/', include('dj_rest_auth.registration.urls')),
-
 ]
--- backend/beers/urls.py
+++ backend/beers/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import SimpleRouter
 from beers.views import BeerByBreweryViewSet, BeerEditorViewSet, BeerViewSet
 
 router = SimpleRouter()
-router.register('by-author', BeerByBreweryViewSet, basename='beers-by-brewery')
+router.register('by-brewery', BeerByBreweryViewSet, basename='beers-by-brewery')
 router.register('editor', BeerEditorViewSet, basename='beers-editor')
 router.register('', BeerViewSet, basename='beers')
 
