41e450f2820ac34f88e5dd0f7d85cc9a57b29d68
Michael Jellinger <jellingermichael@gmail.com>
2024-12-12 10:50:47 +0100

    update beer model

--- backend/beers/models.py
+++ backend/beers/models.py
@@ -4,12 +4,17 @@ from django.db import models
 
 from .validators import validate_title
 
+
 class Beer(models.Model):
     name = models.CharField(max_length=100, validators=[validate_title])
-    description = models.TextField(validators=[RegexValidator(r'^[\w\s.:;\'"]*$')])
     brewery = models.CharField(max_length=100)
+    description = models.TextField(validators=[RegexValidator(r'^[\w\s.:;\'"]*$')])
+    alcohol_content: models.DecimalField(max_digits=5, decimal_places=2)
+    beer_type = models.CharField(max_length=100)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
