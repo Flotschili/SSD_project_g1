5406c63053bb8cfa6d3f3e98690e093eb4d5e055
Simon Silvester Kaufmann <simon.kaufmann99@gmail.com>
2024-12-13 01:47:37 +0100

    Add further implementation of tui

--- tui/beer_hub/logic.py
+++ tui/beer_hub/logic.py
@@ -1,36 +1,57 @@
 from dataclasses import dataclass, field
+from typing import Optional
 
 from beer_hub_client.models import Login
-from valid8 import validate
-
 from beer_hub.domain import Beer, Brewery
 
 
 @dataclass(frozen=True, order=True)
-class BeerHub:
+class InMemoryBeerHub:
     __beers: list[Beer] = field(default_factory=list, init=False, repr=False)
 
     def number_of_beers(self) -> int:
         return len(self.__beers)
 
-    def beer_at_index(self, index) -> Beer:
-        validate('index', index, min_value=0, max_value=len(self.__beers) - 1)
-        return self.__beers[index]
+    def get_beers(self) -> list[Beer]:
+        return self.__beers
+
+    def get_beer_by_id(self, id: int) -> Optional[Beer]:
+        return next(beer for beer in self.__beers if beer.id == id)
+
+    def get_beer_by_name(self, name: str) -> Optional[Beer]:
+        return next(beer for beer in self.__beers if beer.name == name)
 
     def add_beer(self, beer: Beer) -> None:
         self.__beers.append(beer)
 
-    def remove_beer(self, index: int) -> None:
-        self.__beers.pop(index)
+    def update_beer_by_id(self, id: int, beer: Beer) -> None:
+        pass
 
-    def list_of_breweries(self) -> list[Brewery]:
-        return list({beer.brewery for beer in self.__beers})
+    def update_beer_by_name(self, name: str, beer: Beer) -> None:
+        pass
 
-    def beers_of_brewery(self, brewery: Brewery) -> list[Beer]:
+    def delete_beer_by_id(self, id: int) -> None:
+        self.__beers.remove(self.get_beer_by_id(id))
+
+    def delete_beer_by_name(self, name: str) -> None:
+        self.__beers.remove(next(beer for beer in self.__beers if beer.name == name))
+
+    def number_of_breweries(self) -> int:
+        return len(self.get_breweries())
+
+    def get_breweries(self) -> list[Brewery]:
+        return list(set({beer.brewery for beer in self.__beers}))
+
+    def get_beers_by_brewery(self, brewery: Brewery) -> list[Beer]:
         return [beer for beer in self.__beers if beer.brewery == brewery]
 
-    def sort_by_ascending_alcohol_content(self) -> None:
+    def get_beers_by_ascending_alcohol_content(self) -> list[Beer]:
         self.__beers.sort(key=lambda beer: beer.alcohol_content)
+        return self.__beers
+
+    def get_beers_by_descending_alcohol_content(self) -> list[Beer]:
+        self.__beers.sort(key=lambda beer: beer.alcohol_content, reverse=True)
+        return self.__beers
 
     def __iter__(self):
         return iter(self.__beers)
