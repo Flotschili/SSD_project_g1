1a2b552bfefff1832bd4e65b7ac20d6717829ce1
Tobias Kneidinger <tobias.kneidinger@gmail.com>
2024-12-15 16:49:43 +0100

    Some minor changes

--- gui/src/components/Header.tsx
+++ gui/src/components/Header.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
-import AuthService from '../services/AuthService';
-import logo from '../logo_cropped.jpg';
+import React from "react";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import AuthService from "../services/AuthService";
+import logo from "../logo_cropped.jpg";
 
 const Header: React.FC = () => {
   const handleLogout = () => {
@@ -11,10 +11,10 @@ const Header: React.FC = () => {
   };
 
   return (
-    <AppBar position="static" sx={{marginBottom: '1rem'}}>
+    <AppBar position="static" sx={{ marginBottom: "1rem" }}>
       <Toolbar>
         <Box sx={{ flexGrow: 1 }}>
-          <img src={logo} alt="Beer" style={{ height: '3rem' }} />
+          <img src={logo} alt="Beer" style={{ height: "3rem" }} />
         </Box>
         <Button color="inherit" onClick={handleLogout}>
           Logout
---
+++ gui/src/themes/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#212025', // Replace with your primary color
+    },
+    secondary: {
+      main: '#dc004e', // Replace with your secondary color
+    },
+  },
+});
+
+export default theme;
